4cadd4612fc98bba653b3abb9036176820108c46
李智坚 <zhijian.li@hand-china.com>
2019-12-25 13:29:25 +0800

    feat: 登录页

--- go-trip/src/layouts/Header/index.tsx
+++ go-trip/src/layouts/Header/index.tsx
@@ -1,11 +1,9 @@
-import React,{ useEffect, useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import styles from './index.less';
 import { Divider } from 'antd';
 import NavLink from 'umi/navlink';
 
-
 const Header: React.FC = () => {
-
   const [activeBanner, setActiveBanner] = useState('home');
 
   const headerList = new Array(
@@ -24,26 +22,43 @@ const Header: React.FC = () => {
     key: string;
   }
 
-  const renderHeaderList = (): React.ReactNode =>{
-    return (
-      headerList.map((item: headerObject) => (
-        <NavLink to={item.path} onClick={()=>{setActiveBanner(item.key)}} className={item.key===activeBanner? 'active': ''}>  <li key={item.key}>{item.name}</li> </NavLink> 
-      ))
-    )
-  }
+  const renderHeaderList = (): React.ReactNode => {
+    return headerList.map((item: headerObject) => (
+      <li key={item.key} className={item.key === activeBanner ? 'activeBanner' : ''}>
+        <NavLink
+          to={item.path}
+          onClick={() => {
+            setActiveBanner(item.key);
+          }}
+        >
+          {item.name}
+        </NavLink>
+      </li>
+    ));
+  };
 
   return (
     <div className={styles.header}>
       <div className="logo" />
-      <ul className="header-lists">
-        {renderHeaderList()}
-      </ul>
+      <ul className="header-lists">{renderHeaderList()}</ul>
       <div className="login">
-        <NavLink to="/login/" className="log">
+        <NavLink
+          to="/login/"
+          className="log"
+          onClick={() => {
+            setActiveBanner('');
+          }}
+        >
           登录
         </NavLink>
         <Divider type="vertical" style={{ height: '16px' }} />
-        <NavLink to="/register/" className="log">
+        <NavLink
+          to="/register/"
+          className="log"
+          onClick={() => {
+            setActiveBanner('');
+          }}
+        >
           注册
         </NavLink>
       </div>
--- go-trip/src/pages/login/index.tsx
+++ go-trip/src/pages/login/index.tsx
@@ -1,12 +1,74 @@
-import React, { Component, useState } from 'react'
+import React, { Component, useState } from 'react';
+import { Form, Icon, Input, Button, Checkbox } from 'antd';
+import styles from './index.less';
 
-const Login :React.FC = props => {
+const Login: React.FC = props => {
   const [page, setpage] = useState('登录');
-  return(
-   <div>
-     <h1>{page}</h1>
-   </div>
-  )
-} 
 
-export default Login;
+  const { getFieldDecorator, getFieldsValue } = props.form;
+  const FormItem = Form.Item;
+
+  const handleSubmitLogin = () => {
+    props.form.validateFields((err, values) => {
+      if (!err) {
+        const { telephone, password, remember } = getFieldsValue();
+        if (remember) {
+          localStorage.setItem('telephone', telephone);
+          localStorage.setItem('password', password);
+        }
+        alert('登录成功')
+      }
+    });
+  };
+
+  return (
+    <div className={styles.logoWrapper}>
+      <div className="login-form">
+        <Form>
+          <FormItem>
+            {getFieldDecorator('telephone', {
+              rules: [
+                { required: true, message: '请输入手机号!' },
+                {
+                  pattern: /^134[0-8]\d{7}$|^13[^4]\d{8}$|^14[5-9]\d{8}$|^15[^4]\d{8}$|^16[6]\d{8}$|^17[0-8]\d{8}$|^18[\d]{9}$|^19[8,9]\d{8}$/,
+                  message: '请输入正确的手机号',
+                },
+              ],
+              initialValue: localStorage.getItem('telephone')
+            })(<Input placeholder="请输入您的手机号" />)}
+          </FormItem>
+          <FormItem>
+            {getFieldDecorator('password', {
+              rules: [
+                { required: true, message: '请输入密码!' },
+                {
+                  pattern: /^(?![0-9]+$)(?![a-zA-Z]+$)(?![a-z]+$)(?![!@#$%^&*=]+$)[0-9A-Za-z!@#$%^&*=]{6,30}$/,
+                  message: '密码至少包含数字/字母/字符2种组合,长度为6-30个字符',
+                },
+              ],
+              initialValue: localStorage.getItem('password')
+            })(<Input.Password placeholder="请输入密码" />)}
+          </FormItem>
+          <FormItem>
+            {getFieldDecorator('remember', {
+              valuePropName: 'checked',
+              initialValue: true,
+            })(<Checkbox>记住我</Checkbox>)}
+            <a className="login-form-forgot" href="">
+              忘记密码
+            </a>
+            <Button type="primary" className="login-form-button" onClick={handleSubmitLogin}>
+              登录
+            </Button>
+            点我<a href="/register/">立即注册</a>{' '}
+            <span style={{ marginLeft: '40px' }}>
+              或者<a href="/">返回首页</a>
+            </span>
+          </FormItem>
+        </Form>
+      </div>
+    </div>
+  );
+};
+
+export default Form.create({ name: 'goTrip_login' })(Login);
